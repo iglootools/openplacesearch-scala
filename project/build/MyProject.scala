56ae2baaa4f83c59d3734de1bd1c333aaf869195
Sami Dalouche <sami.dalouche@gmail.com>
2011-01-26 20:37:25 -0500

    added idea plugin to SBT

--- project/build/MyProject.scala
+++ project/build/MyProject.scala
@@ -1,5 +1,6 @@
 import sbt._
 import de.element34.sbteclipsify._
  
-class MyProject(info: ProjectInfo) extends DefaultProject(info) with Eclipsify {
+class MyProject(info: ProjectInfo) extends DefaultProject(info) with Eclipsify with IdeaProject {
+    
 }
