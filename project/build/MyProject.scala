56f0d469160c3f918bf091027aafd9aee5e5dafb
Sami Dalouche <sami.dalouche@gmail.com>
2011-01-26 19:25:55 -0500

    created project skeleton

---
+++ project/build/MyProject.scala
@@ -0,0 +1,5 @@
+import sbt._
+import de.element34.sbteclipsify._
+ 
+class MyProject(info: ProjectInfo) extends DefaultProject(info) with Eclipsify {
+}
---
+++ src/main/scala/com/sirika/openplacesearch/api/Hi.scala
@@ -0,0 +1,7 @@
+package com.sirika.openplacesearch.api
+
+object Hi {
+  def main(args : Array[String]) : Unit = {
+      println("Hi")
+  }
+}
