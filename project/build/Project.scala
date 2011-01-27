e3c9c62cf9b022fbd19bb83c6db095433c1b1dd8
Sami Dalouche <sami.dalouche@gmail.com>
2011-01-27 14:41:47 -0500

    now depends on all anticipated dependencies

---
+++ project/build/Project.scala
@@ -0,0 +1,29 @@
+import sbt._
+import de.element34.sbteclipsify._
+ 
+class Project(info: ProjectInfo) extends DefaultProject(info) with Eclipsify with IdeaProject {
+    //val mavenLocal = "Local Maven Repository" at "file://"+Path.userHome+"/.m2/repository"
+    val geotoolsRepository = "Open Source Geospatial Foundation Repository" at "http://download.osgeo.org/webdav/geotools/"
+    val javanetRepository = "Java.net Repository" at "http://download.java.net/maven/2"
+    
+    // commons
+    val jodaTime = "joda-time" % "joda-time" % "1.6.2" withSources()
+    val icu4j = "com.ibm.icu" % "icu4j" % "4.6" withSources()
+    val slf4jApi = "org.slf4j" % "slf4j-api" % "1.6.1" withSources()
+    
+    // HTTP and REST
+    val HttpComponentsVersion = "4.1"
+    val hcHelpers = "com.sirika.hchelpers" % "hchelpers-core" % "0.8" withSources()
+    val httpComponentsClient = "org.apache.httpcomponents" % "httpclient" % HttpComponentsVersion withSources()
+    val httpComponentsMime = "org.apache.httpcomponents" % "httpmime" % HttpComponentsVersion withSources()
+    
+    
+    // GIS
+    val jts = "com.vividsolutions" % "jts" % "1.8" // withSources()
+    val geotools = "org.geotools" % "gt-main" % "2.6.5" withSources() // depends geotools and java.net repositories
+    
+    // test dependencies
+    val junit = "junit" % "junit" % "4.8.2" % "test" withSources()
+    val scalaTest = "org.scalatest" % "scalatest" % "1.2"  % "test" withSources()
+    val mockito = "org.mockito" % "mockito-all" % "1.8.5" % "test" withSources()
+}
