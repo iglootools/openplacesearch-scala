cf1523a702355de95f00a140038007908f55f130
Sami Dalouche <sami.dalouche@gmail.com>
2011-01-28 09:59:26 -0500

    more spec coverage for language

--- src/test/scala/com/sirika/openplacesearch/api/language/LanguageSpec.scala
+++ src/test/scala/com/sirika/openplacesearch/api/language/LanguageSpec.scala
@@ -10,41 +10,47 @@ import com.sirika.openplacesearch.api.language._
 
 @RunWith(classOf[JUnitRunner])
 class LanguageSpec extends Spec with ShouldMatchers {
-    describe("toString ") {
-        it("should contain name, alpha3 and alpha2 for French") {
-            french.toString should be === "Language{name=Français, alpha3=fra, alpha2=Some(fr)}"
-        }
-        
-        it("should contain name, alpha3 for Hawaiian") {
-            hawaiian.toString should be === "Language{name=Hawaiian, alpha3=haw, alpha2=None}"
-        }
-    }
-    
     describe("Language") {
-        it("should require a non-empty alpha3 code") {
-            evaluating { Language(name="Hawaiian", alpha3Code="") } should produce [IllegalArgumentException]
+        describe("constructor") {
+            it("should require a non-empty alpha3 code") {
+                evaluating { Language(name="Hawaiian", alpha3Code="") } should produce [IllegalArgumentException]
+            }
+
+            it("should require a non-empty name") {
+                evaluating { Language(name="", alpha3Code="haw") } should produce [IllegalArgumentException]
+            }
+            
+            it("should require a non-null alpha2 code") {
+                evaluating { Language(name="Hawaiian", "haw", null) } should produce [IllegalArgumentException]
+            }
         }
-        
-        it("should require a non-empty name") {
-            evaluating { Language(name="", alpha3Code="haw") } should produce [IllegalArgumentException]
+
+        describe("toString") {
+            it("should return name, alpha3 and alpha2 for French") {
+                french.toString should be === "Language{name=Français, alpha3=fra, alpha2=Some(fr)}"
+            }
+
+            it("should return name, alpha3 for Hawaiian") {
+                hawaiian.toString should be === "Language{name=Hawaiian, alpha3=haw, alpha2=None}"
+            }
         }
     }
     
-    describe("A given Language") {
-        it("should be equal to another language that has the same alpha3Code") {
-            french should be === french
+    describe("A given language") {
+            it("should be equal to another language that has the same alpha3Code") {
+                french should be === french
+            }
+
+            it("should have the same hashcode as another language that has the same alpha3Code") {
+                french.hashCode should be === french.hashCode
+            }
+
+            it("should not be equal to another language that has a different alpha3Code") {
+                french should not be hawaiian
+            }
+
+            it("should not have the same hashcode as another language that has a different alpha3Code") {
+                french.hashCode should not be hawaiian.hashCode
+            }
         }
-        
-        it("should have the same hashcode as another language that has the same alpha3Code") {
-            french.hashCode should be === french.hashCode
-        }
-        
-        it("should not be equal to another language that has a different alpha3Code") {
-            french should not be hawaiian
-        }
-        
-        it("should not have the same hashcode as another language that has a different alpha3Code") {
-            french.hashCode should not be hawaiian.hashCode
-        }
-    }
 }
--- src/test/scala/com/sirika/openplacesearch/api/language/Languages.scala
+++ src/test/scala/com/sirika/openplacesearch/api/language/Languages.scala
@@ -1,7 +1,6 @@
 package com.sirika.openplacesearch.api.language
 
 object Languages {
-
     def french(): Language = Language(name = "Français", alpha3Code = "fra", alpha2Code = Some("fr"))
     def english(): Language = Language(name = "English", alpha3Code = "eng", alpha2Code = Some("en"))
     def spanish(): Language = Language(name = "Spanish", alpha3Code = "spa", alpha2Code = Some("es"))
