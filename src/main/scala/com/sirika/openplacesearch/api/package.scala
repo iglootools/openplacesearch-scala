21d3ba48cc46da84fb070358070d63b9dadbb671
Sami Dalouche <sami.dalouche@gmail.com>
2011-01-31 17:16:15 -0500

    reduced the package-level properties visibility

--- src/main/scala/com/sirika/openplacesearch/api/package.scala
+++ src/main/scala/com/sirika/openplacesearch/api/package.scala
@@ -3,9 +3,9 @@ package com.sirika.openplacesearch
 import com.vividsolutions.jts.geom.{PrecisionModel, GeometryFactory, Envelope, Geometry}
 
 package object api {
-  val WGS84_SRID = 4326
-  val WGS84_ENVELOPE = new Envelope(-180.0, 180.0, -90.0, 90.0)
-  val Precision: PrecisionModel = new PrecisionModel(PrecisionModel.FLOATING)
-  val GeometryFactory: GeometryFactory = new GeometryFactory(Precision, WGS84_SRID)
-  val ValidCoordinateBounds: Geometry = api.GeometryFactory.toGeometry(WGS84_ENVELOPE);
+  private[api] val WGS84_SRID = 4326
+  private[api] val WGS84_ENVELOPE = new Envelope(-180.0, 180.0, -90.0, 90.0)
+  private[api] val Precision: PrecisionModel = new PrecisionModel(PrecisionModel.FLOATING)
+  protected[api] val GeometryFactory: GeometryFactory = new GeometryFactory(Precision, WGS84_SRID)
+  protected[api] val ValidCoordinateBounds: Geometry = api.GeometryFactory.toGeometry(WGS84_ENVELOPE);
 }
