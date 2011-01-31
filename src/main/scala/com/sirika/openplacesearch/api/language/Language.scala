2746bf1027bb0fecfadbb21e41fa3806649b1d6d
Sami Dalouche <sami.dalouche@gmail.com>
2011-01-31 11:08:09 -0500

    improved checks in Language

--- src/main/scala/com/sirika/openplacesearch/api/language/Language.scala
+++ src/main/scala/com/sirika/openplacesearch/api/language/Language.scala
@@ -4,15 +4,15 @@ import com.google.common.base.Objects
 import com.google.common.base.Strings
 
 object Language {
-    def apply(name : String, alpha3Code : String, alpha2Code : Option[String]=None) = new Language(name, alpha3Code, alpha2Code)
-    def unapply(l : Language) = Some(l.name, l.alpha3Code, l.alpha2Code )
+  def apply(name : String, alpha3Code : String, alpha2Code : Option[String]=None) = new Language(name, alpha3Code, alpha2Code)
+  def unapply(l : Language) = Some(l.name, l.alpha3Code, l.alpha2Code )
 }
 
 /**
  * ISO 639 Language (639-1 -alpha2- and 639-2 -alpha3-)
- * 
+ *
  * Codes are written lowercase !!! And ISO 639-2 codes are not considered here
- * 
+ *
  * @param alpha2Code <a href="http://en.wikipedia.org/wiki/ISO_639-1">ISO 639-1</a>
  * @param alpha3Code <a href="http://en.wikipedia.org/wiki/ISO_639-1">ISO 639-3</a>
  * @author Sami Dalouche (sami.dalouche@gmail.com)
@@ -20,20 +20,21 @@ object Language {
  * @see <a href="http://en.wikipedia.org/wiki/ISO_639-3">ISO 639-3</a>
  */
 final class Language(val name : String, val alpha3Code : String, val alpha2Code : Option[String]) {
-    require(Option(name) exists (_.nonEmpty), "name is required")
-    require(Option(alpha3Code) exists (_.nonEmpty), "alpha3Code is required")
-    require(alpha2Code != null, "alpha2Code must be a non-null Option")
-    
-    override def hashCode(): Int = Objects.hashCode(name, alpha3Code, alpha2Code)
-    
-    override def equals(other: Any): Boolean = other match {
-        case Language(`name`, `alpha3Code`, `alpha2Code`) => true
-        case _ => false
-    }
-    
-    override def toString() : String = Objects.toStringHelper(this)
-        .add("name", name)    
-        .add("alpha3", alpha3Code)
-        .add("alpha2", alpha2Code)
-        .toString()
+  require(Option(name) exists (_.nonEmpty), "name is required")
+  require(Option(alpha3Code) exists {c : String => c.nonEmpty && c.length == 3}, "alpha3Code is required, and should consist of 3 characters")
+  require(alpha2Code != null, "alpha2Code must be a non-null Option")
+  alpha2Code foreach { c : String => require(c.length == 2, "alpha2Code is be 2-character long")}
+
+  override def hashCode(): Int = Objects.hashCode(name, alpha3Code, alpha2Code)
+
+  override def equals(other: Any): Boolean = other match {
+    case Language(`name`, `alpha3Code`, `alpha2Code`) => true
+    case _ => false
+  }
+
+  override def toString() : String = Objects.toStringHelper(this)
+    .add("name", name)
+    .add("alpha3", alpha3Code)
+    .add("alpha2", alpha2Code)
+    .toString()
 }
--- src/test/scala/com/sirika/openplacesearch/api/language/LanguageSpec.scala
+++ src/test/scala/com/sirika/openplacesearch/api/language/LanguageSpec.scala
@@ -11,33 +11,41 @@ import com.sirika.openplacesearch.api.language._
 
 @RunWith(classOf[JUnitRunner])
 class LanguageSpec extends Spec with ShouldMatchers {
-    describe("constructor") {
-        it("should require a non-empty alpha3 code") {
-            evaluating { Language(name="Hawaiian", alpha3Code="") } should produce [IllegalArgumentException]
-        }
-
-        it("should require a non-empty name") {
-            evaluating { Language(name="", alpha3Code="haw") } should produce [IllegalArgumentException]
-        }
-        
-        it("should require a non-null alpha2 code") {
-            evaluating { Language(name="Hawaiian", "haw", null) } should produce [IllegalArgumentException]
-        }
+  describe("constructor") {
+    it("should require a non-empty alpha3 code") {
+      evaluating { Language(name="Hawaiian", alpha3Code="") } should produce [IllegalArgumentException]
     }
 
-    describe("toString") {
-        it("should return name, alpha3 and alpha2 for French") {
-            french.toString should be === "Language{name=French, alpha3=fra, alpha2=Some(fr)}"
-        }
+    it("should require a 3-character alpha3 code") {
+      evaluating { Language(name="French", alpha3Code = "fr") } should produce[IllegalArgumentException]
+    }
 
-        it("should return name, alpha3 for Hawaiian") {
-            hawaiian.toString should be === "Language{name=Hawaiian, alpha3=haw, alpha2=None}"
-        }
+    it("should require a non-empty name") {
+      evaluating { Language(name="", alpha3Code="haw") } should produce [IllegalArgumentException]
     }
-    
-    describe("hashCode and equals") {
-        it("should be sane") {
-            SanityChecks.hashCodeAndEqualsShouldBeSane(french, eqObj=french, neObj=hawaiian)
-        }
+
+    it("should require a non-null alpha2 code") {
+      evaluating { Language(name="Hawaiian", "haw", null) } should produce [IllegalArgumentException]
     }
+
+    it("should require a 2-character alpha2 code") {
+      evaluating { Language(name="French", alpha3Code = "fra", alpha2Code=Some("fra")) } should produce[IllegalArgumentException]
+    }
+  }
+
+  describe("toString") {
+    it("should return name, alpha3 and alpha2 for French") {
+      french.toString should be === "Language{name=French, alpha3=fra, alpha2=Some(fr)}"
+    }
+
+    it("should return name, alpha3 for Hawaiian") {
+      hawaiian.toString should be === "Language{name=Hawaiian, alpha3=haw, alpha2=None}"
+    }
+  }
+
+  describe("hashCode and equals") {
+    it("should be sane") {
+      SanityChecks.hashCodeAndEqualsShouldBeSane(french, eqObj=french, neObj=hawaiian)
+    }
+  }
 }
