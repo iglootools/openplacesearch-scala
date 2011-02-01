5372e948970cec60625afe3e3119a3b0caefbe03
Sami Dalouche <sami.dalouche@gmail.com>
2011-02-01 17:20:23 -0500

    added getCountry / CountryProvider to AdministrativeEntity

--- src/main/scala/com/sirika/openplacesearch/api/administrativedivision/AdministrativeEntity.scala
+++ src/main/scala/com/sirika/openplacesearch/api/administrativedivision/AdministrativeEntity.scala
@@ -6,12 +6,12 @@ import com.sirika.openplacesearch.api.feature.{FeatureNameProvider, ParentAdmini
  * @author Sami Dalouche (sami.dalouche@gmail.com)
  */
 
-trait AdministrativeEntity extends ParentAdministrativeEntityProvider with FeatureNameProvider {
+trait AdministrativeEntity extends ParentAdministrativeEntityProvider with FeatureNameProvider with CountryProvider {
   /**
    * ADM0 (Country), ADM1 (First Level ADM), ADM2, ...)
    */
   def administrativeDivisionLevel: Int = parentAdministrativeEntity.map {_.administrativeDivisionLevel}.getOrElse(-1) + 1
-  def parentAdministrativeEntityW(level: Int): AdministrativeEntity = {
+  def parentAdministrativeEntityHavingLevel(level: Int): AdministrativeEntity = {
     val currentLevel = administrativeDivisionLevel
     require(level <= currentLevel && level >= 0, "requested level should be [0-%d]. 0 = Country, 1 = ADM1, 2 = ADM2, ...)".format(currentLevel))
 
@@ -19,7 +19,16 @@ trait AdministrativeEntity extends ParentAdministrativeEntityProvider with Featu
       case l if l == currentLevel => this
       case _ =>
         assume(parentAdministrativeEntity != None)
-        parentAdministrativeEntity.map {_.parentAdministrativeEntityW(level) }.get
+        parentAdministrativeEntity.map {_.parentAdministrativeEntityHavingLevel(level) }.get
+    }
+  }
+
+  // CurrencyProvider
+  def country: Country = {
+    val administrativeEntityAtLevelZero = parentAdministrativeEntityHavingLevel(0)
+    administrativeEntityAtLevelZero match {
+      case c: Country =>  c.asInstanceOf[Country]
+      case _ => throw new RuntimeException("By design, ADM0 should be a country")
     }
   }
 }
