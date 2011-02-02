0261f477d75c436ab9836ecdb5b1f234864619ad
Sami Dalouche <sami.dalouche@gmail.com>
2011-02-02 14:24:52 -0500

    added Administrative Division

---
+++ src/main/scala/com/sirika/openplacesearch/api/administrativedivision/AdministrativeDivision.scala
@@ -0,0 +1,24 @@
+package com.sirika.openplacesearch.api.administrativedivision
+
+import com.sirika.openplacesearch.api.language.Language
+import com.sirika.openplacesearch.api.feature.{FeatureNameProvider, LocalizedName, ParentAdministrativeEntityProvider}
+
+/**
+ * @param code represents the <a href="http://en.wikipedia.org/wiki/List_of_FIPS_region_codes">FIPS code</a> for US states.
+ * @author Sami Dalouche (sami.dalouche@gmail
+ */
+final case class AdministrativeDivision(
+  val code: String,
+  val name: String,
+  val featureNameProvider: FeatureNameProvider,
+  val parentAdministrativeEntityProvider: ParentAdministrativeEntityProvider) extends AdministrativeEntity {
+
+  // FeatureNameProvider
+  def shortName(language: Language): String = featureNameProvider.shortName(language)
+  def preferredName(language: Language): String = featureNameProvider.preferredName(language)
+  def localizedNames: List[LocalizedName] = featureNameProvider.localizedNames
+  def userFriendlyCode: Option[String] = featureNameProvider.userFriendlyCode
+
+  // ParentAdministrativeEntityProvider
+  def parentAdministrativeEntity: Option[AdministrativeEntity] = parentAdministrativeEntityProvider.parentAdministrativeEntity
+}
