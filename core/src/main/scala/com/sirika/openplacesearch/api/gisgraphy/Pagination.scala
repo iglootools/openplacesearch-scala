d5ad6904a3dadb5486522f38c29501ba98e1ecaa
Sami Dalouche <sami.dalouche@gmail.com>
2011-02-14 16:43:56 -0500

    got rid of pagination limitation to 10 results (we'll patch gisgraphy)

--- core/src/main/scala/com/sirika/openplacesearch/api/gisgraphy/Pagination.scala
+++ core/src/main/scala/com/sirika/openplacesearch/api/gisgraphy/Pagination.scala
@@ -3,7 +3,6 @@ package com.sirika.openplacesearch.api.gisgraphy
 final case class Pagination(val firstResult: Int, val numberOfResults: Int) {
   require(firstResult >=1, "the first result is 1")
   require(numberOfResults >=1, "we cannot retrieve less than 1 result")
-  require(numberOfResults <= 10, "stupid gisgraphy does not support more than 10 results at the same time")
 
   def endResult: Int = firstResult + numberOfResults -1
 }
